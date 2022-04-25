5db3c761b1aaa990e84b87ad3fe2a5dc86f77883
Esauh <esau.hutcherson@bison.howard.edu>
2022-04-25 02:06:17 -0400

    Commit
    Creates get_input procedure that gets the number of char to read from input then gets the space for the input

--- Project 3/project3.s
+++ Project 3/project3.s
@@ -11,3 +11,9 @@ error: .asciiz "-" #invalid input message
 main:
 addi $s0, $zero, 29
 addi $s1, $zero, 19 #bases used for rest of program
+
+get_input:
+li $v0, 8 #number of char to read from input
+la $a0, str #space for input
+li $a1, 1001
+syscall
