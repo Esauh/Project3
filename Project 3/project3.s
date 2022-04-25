cc61644bca5819b4cd0d2cb18586aa32760099db
Esauh <esau.hutcherson@bison.howard.edu>
2022-04-25 01:18:03 -0400

    Commit
    added rest of beginning program including main which saved my bases 29 and 19 since the formula #02936343 % 11 = 3 making it 26+3 = 29

--- Project 3/project3.s
+++ Project 3/project3.s
@@ -4,3 +4,10 @@
 .data
 
 str: .space 1001 #makes space for the 1000 char input
+error: .asciiz "-" #invalid input message
+
+.text
+
+main:
+addi $s0, $zero, 29
+addi $s1, $zero, 19 #bases used for rest of program
